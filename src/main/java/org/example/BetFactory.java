e00174cbb25d047af22186cf5e06f6e2616f6c87
awvalent11 <awvalent11@gmail.com>
2024-09-16 14:57:56 -0500

    Comments out if clause in MoneylineBuilding so code complies

---
+++ src/main/java/org/example/BetFactory.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.example.Bet;
+import org.example.EnemyShipFactory;
+
+public interface BetFactory {
+
+    public Game addGameData();
+    public Wager addWagerData();
+
+}
--- src/main/java/org/example/MoneylineBuilding.java
+++ src/main/java/org/example/MoneylineBuilding.java
@@ -7,25 +7,27 @@ public class MoneylineBuilding extends BetBuilding{
 
     @Override
     protected Bet makeBet(BetType betType){
-        EnemyShip theEnemyShip = null;
+
+        Bet bet = null;
         //If UFO was sent to grab use the factory that knows
         //what types of weapons and engines a regular UFO
         //needs. The EnemyShip object is returned & given a name
 
 
         //should these if statements be sport and bet type for the different building classes?
-        if(betType.equals("Moneyline")){
-            EnemyShipFactory shipPartsFactory = new UFOEnemyShipFactory();
-            theEnemyShip = new UFOEnyShip(shipPartsFactory);
-            theEnemyShip.setName("First regular UFO Enemy Ship");
-        }
+//        if(betType.equals("Moneyline")){
+//            BetFactory betFactory = new BetFactory() {
+//            }
+//            bet = new UFOEnyShip(shipPartsFactory);
+//            theEnemyShip.setName("First regular UFO Enemy Ship");
+//        }
+//
+//        if(betType.equals("Moneyline")){
+//            EnemyShipFactory shipFactory = new BetFactory();
+//            theEnemyShip = new UFOBossEnemyShip(shipFactory);
+//        }
+        return bet;
 
-        if(betType.equals("Moneyline")){
-            EnemyShipFactory shipFactory = new UFOEnemyShipFactory();
-            theEnemyShip = new UFOBossEnemyShip(shipFactory);
-        }
-
-        return theEnemyShip;
     }
 
 }
